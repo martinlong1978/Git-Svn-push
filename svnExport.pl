64c408ab482571579c127745b2c04ef28c85a2d0
martinlong1978 <martin@longhome.co.uk>
2011-04-14 22:25:00 +0100

    Allow separate config for each repo, and strict processing

--- svnExport.pl
+++ svnExport.pl
@@ -1,17 +1,20 @@
 #!/usr/bin/perl
-
+use strict;
 use File::Path;
 use File::Basename;
 use File::Copy::Recursive;
 
-$SYNC_BASE="/home/martin/gittest";
-$GIT_ROOT="$SYNC_BASE/GIT";
-$SVN_ROOT="$SYNC_BASE/SVNtmp";
-$COMMIT_MESG="$SYNC_BASE/SVNtmp/messages";
-$SVN_BASE_URL="http://www.rozel.net/svn/martin";
-$REMOTE="origin";
-#Needs to be per-project
-@BRANCH_ORDER=("origin/master", "origin/2.8.0", "origin/2.8.1", "origin/2.8.2", "origin/2.8.3");
+my $SYNC_BASE="/home/martin/gittest";
+my $GIT_ROOT="$SYNC_BASE/GIT";
+my $SVN_ROOT="$SYNC_BASE/workingdata";
+my $COMMIT_MESG="$SVN_ROOT/messages";
+my $REMOTE="origin";
+my $SVN_BASE_URL;
+my @BRANCH_ORDER;
+
+# These are loaded per-project
+my @BRANCH_ORDER;
+my $SVN_BASE_URL;
 
 sub branchfromparent
 {
@@ -19,8 +22,8 @@ sub branchfromparent
 	my $branch=$_[1];
 	my $br_parent=$_[2];
 	my $revision=$_[3];
-	my $svn_from="$SVN_BASE_URL/$project/$br_parent/trunk";
-	my $svn_to="$SVN_BASE_URL/$project/$branch/trunk";
+	my $svn_from="$SVN_BASE_URL/$br_parent/trunk";
+	my $svn_to="$SVN_BASE_URL/$branch/trunk";
 	
 	print "Branching $branch from $br_parent at rev $revision\n";
 
@@ -34,11 +37,11 @@ sub branchfromparent
 	#Branch and checkout working copy
 	print "svn copy --parents  $svn_from\@$svnrev $svn_to -m \"Branch for $branch\"";
 	system("svn copy --parents  $svn_from\@$svnrev $svn_to -m \"Branch for $branch\"") == 0
-		or die "Could not connect to $svn_url";
+		or die "Could not create branch";
 	mkpath "$SVN_ROOT/$project/$1";
 	chdir "$SVN_ROOT/$project";
 	system("svn co $svn_to $branch") == 0
-		or die "Could not connect to $svn_url";
+		or die "Could not checkout branch";
 
 }
 
@@ -47,7 +50,7 @@ sub createfirst
 	my $project=$_[0];
 	my $branch=$_[1];
 	my $revision=$_[2];
-	my $svn_url="$SVN_BASE_URL/$project/$branch/trunk";
+	my $svn_url="$SVN_BASE_URL/$branch/trunk";
 
 	print "New project branch: $branch\n";
 	
@@ -72,26 +75,25 @@ sub findparent
 	my $project=$_[1];
 	print "Looking for parent of: $branch\n";
 	open(REVS, "git rev-list --first-parent remotes/$branch |") or die "Broken";
-	@revisions=<REVS>;
+	my @revisions=<REVS>;
 	close(REVS);
 	
 	open(REVCACHE, "$SVN_ROOT/$project.revcache");
 	my %revcache = ();
-	for $cacheentry (<REVCACHE>)
+	for my $cacheentry (<REVCACHE>)
 	{
 		chomp($cacheentry);
-		@entry = split(/ /, $cacheentry);
+		my @entry = split(/ /, $cacheentry);
 		$revcache{$entry[2]}=$entry[1];
 	}
 	close(REVCACHE);
 	
-	for $revision (@revisions)
+	my $rev_last;
+	for my $revision (@revisions)
 	{
 		chomp($revision);
 		print "Looking for parent in rev: $revision\n";
-		#$br_parent=`cat $SVN_ROOT/revcache |grep $revision | cut -d " " -f 2`;
-		$br_parent=$revcache{$revision};
-		#$br_parent=~ /s\n//;
+		my $br_parent=$revcache{$revision};
 		if($br_parent ne "")
 		{
 			&branchfromparent($project, $branch, $br_parent, $revision);
@@ -113,15 +115,17 @@ sub processproject
 		or die "GIT fetch failed.";
 	
 	open(REFS, "git for-each-ref --format=\"\%(refname:short)\" refs/remotes/$REMOTE |grep -v HEAD|");
-	@allbranches=<REFS>;
+	my @allbranches=<REFS>;
 	close(REFS);
 	
-	for $branch (@BRANCH_ORDER)
+	my @branches=();
+	
+	for my $branch (@BRANCH_ORDER)
 	{
 		push(@branches, $branch);
 	}
 	
-	for $branch (@allbranches)
+	for my $branch (@allbranches)
 	{
 		chomp($branch);
 		if(!grep($branch eq $_, @BRANCH_ORDER))
@@ -132,7 +136,7 @@ sub processproject
 
 	print "Branches: @branches\n";
 	
-	for $branch (@branches)
+	for my $branch (@branches)
 	{
 		&processbranch($project, $branch);
 	}
@@ -147,11 +151,11 @@ sub clearsvndir
 	chdir $svndir or die "Couldn't cd to $svndir\n";
 	
 	system("svn up") == 0
-		or die "Could not connect to $svn_url";
+		or die "Could not update working copy";
 	
 	# Delete any files that have been deleted in GIT (but dont delete .svn dirs)
 	open(DELFILES, "find . |grep -v .svn|");
-	for $svnfile (<DELFILES>)
+	for my $svnfile (<DELFILES>)
 	{
 		chomp($svnfile);
 		$svnfile=~s/^\.\///;
@@ -177,7 +181,7 @@ sub processbranch
 
 	chdir "$GIT_ROOT/$project" or die "Can't change to project directory: $project";
 	
-	$tag=`git tag -l svnbranch/$branch | wc -l`;
+	my $tag=`git tag -l svnbranch/$branch | wc -l`;
 	chomp($tag);
 	if($tag < 1)
 	{
@@ -186,17 +190,17 @@ sub processbranch
 	
 	chdir "$GIT_ROOT/$project" or die "Can't change to project directory: $project";
 	
-	$lastrev=`git show-ref -s --dereference svnbranch/$branch`;
+	my $lastrev=`git show-ref -s --dereference svnbranch/$branch`;
 	chomp($lastrev);
 	
 	print "Last revision synced: $lastrev\n";
 	
-	$svndir="$SVN_ROOT/$project/$branch";
+	my $svndir="$SVN_ROOT/$project/$branch";
 	
 	mkpath $svndir;
 	
 	open(BRANCHREVS, "git rev-list --first-parent --reverse ${lastrev}..${branch}|");
-	for $revision (<BRANCHREVS>)
+	for my $revision (<BRANCHREVS>)
 	{
 		chomp($revision);
 		print "Preparing to write revision $revision\n";
@@ -217,12 +221,12 @@ sub processbranch
 			or die "Could not remove .git dir from svn working copy";
 
 		open(TOADD, "svn status |grep \?|");
-		for $path (<TOADD>)
+		for my $path (<TOADD>)
 		{
 			chomp($path);
 			if ("$path" ne "?" )
 			{
-				@fields=split(/\s+/,"$path");
+				my @fields=split(/\s+/,"$path");
 				system("svn add \"$fields[1]\"") == 0
 					or die "Could not add files to svn index";
 			}
@@ -232,9 +236,9 @@ sub processbranch
 		system("svn commit -F $COMMIT_MESG/$revision") == 0
 			or die "Commit failed.";
 		system("svn up") == 0
-			or die "Could not connect to $svn_url";
+			or die "Couldn't update SVN working copy";
 		
-		$svnrev=`svn info |grep 'Last Changed Rev' | cut -d " " -f 4`;
+		my $svnrev=`svn info |grep 'Last Changed Rev' | cut -d " " -f 4`;
 		chomp($svnrev);
 		
 		open(REVCACHE, ">>$SVN_ROOT/$project.revcache");
@@ -249,12 +253,47 @@ sub processbranch
 	close(BRANCHREVS);
 }
 
+sub parse_config_file {
+
+    my ($config_line, $Name, $Value, $Config, $File);
+
+    ($File, $Config) = @_;
+    
+    print "Loading $File\n";
+
+    open (CONFIG, "$File")
+	    or die "ERROR: Config file not found : $File";
+
+    while (<CONFIG>) {
+        $config_line=$_;
+        chomp ($config_line);          # Get rid of the trailling \n
+        $config_line =~ s/^\s*//;     # Remove spaces at the start of the line
+        $config_line =~ s/\s*$//;     # Remove spaces at the end of the line
+        if ( ($config_line !~ /^#/) && ($config_line ne "") ){    # Ignore lines starting with # and blank lines
+            ($Name, $Value) = split (/=/, $config_line);          # Split each line into name value pairs
+            $$Config{$Name} = $Value;                             # Create a hash of the name value pairs
+        }
+    }
+
+    close(CONFIG);
+
+}
+
 sub doimport
 {
 	mkpath $COMMIT_MESG;
 	
-	for $project (glob "$GIT_ROOT/*")
+	for my $projectconfig (glob "$GIT_ROOT/*.config")
 	{
+		my $project=$projectconfig;
+		my %config;
+		$project=~s/(.*)\.config/\1/;
+		&parse_config_file($projectconfig,\%config);
+		
+		$SVN_BASE_URL=$config{"SVN_URL"};
+		my $BRANCHES=$config{"BRANCH_ORDER"};
+		@BRANCH_ORDER=split(",", $BRANCHES);
+		
 		&processproject(basename($project));
 	}
 
